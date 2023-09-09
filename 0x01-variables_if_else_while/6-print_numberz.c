00054f83cebfb70024d3675cfaebeef2bbf2b3e4
olufayokunmi oshinnuga <osinugafayo@gmail.com>
2023-09-09 07:45:26 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
